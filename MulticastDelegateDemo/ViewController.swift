b6a07d8ed5d6e85f702df549669a88de669a3a19
João Nunes <joao3001@hotmail.com>
2015-12-28 17:37:20 +0200

    Initial Commit

---
+++ MulticastDelegateDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MulticastDelegateDemo
+//
+//  Created by Joao Nunes on 28/12/15.
+//  Copyright © 2015 Joao Nunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
