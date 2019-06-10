3ad6ff85c7ccd3ed266f53a4e875bc40ae3de341
Anton Sokolchenko <wsevendays@gmail.com>
2019-06-10 15:56:20 +0300

    Added a fixed target

---
+++ MulticastDelegateSwift iOS/MulticastDelegateSwift_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MulticastDelegateSwift_iOS.h
+//  MulticastDelegateSwift iOS
+//
+//  Created by Anton Sokolchenko on 6/10/19.
+//  Copyright © 2019 Joao Nunes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MulticastDelegateSwift_iOS.
+FOUNDATION_EXPORT double MulticastDelegateSwift_iOSVersionNumber;
+
+//! Project version string for MulticastDelegateSwift_iOS.
+FOUNDATION_EXPORT const unsigned char MulticastDelegateSwift_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MulticastDelegateSwift_iOS/PublicHeader.h>
+
+
