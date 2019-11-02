8826c0979f950acbee92738848de5e8defd59d17
Joao Nunes <joao3001@hotmail.com>
2019-11-02 11:22:21 +0200

    Updated package to support xcode 11.x

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "MulticastDelegate"
+    name: "MulticastDelegate",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v9), .watchOS(.v2), .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "MulticastDelegate", targets: ["MulticastDelegate"])
+    ],
+    targets: [
+        .target(name: "MulticastDelegate")
+    ]
 )
